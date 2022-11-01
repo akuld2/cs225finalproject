310fa03772d808011b10da8aa034df0da1c5a42a
Megan Ja <meganja2@illinois.edu>
2022-11-01 13:27:09 +0000

    added documentation to the edge class

--- src/Edge.h
+++ src/Edge.h
@@ -1,15 +1,46 @@
 #pragma once
 
-class Edge {
-    public:
-        double getFare();
-        double getMiles();
-        double getTime();
-        void averageFare(double newFare);
-        void averageMiles(double newMiles);
-        void averageTime(double newTime);
-    private:
-        double fare_;
-        double miles_;
-        double time_;
+class Edge
+{
+public:
+    /**
+     * getFare will get the fare of the trip
+     * @return fare_
+     */
+    double getFare();
+
+    /**
+     * getMiles will return the miles travelled
+     * @return miles_
+     */
+    double getMiles();
+
+    /**
+     * getTime will return the time of the trip
+     * @return time_
+     */
+    double getTime();
+
+    /**
+     * averageFare will input the new fare and will average it with the existing fare
+     * @param newFare is the fare we need to include in the average
+     */
+    void averageFare(double newFare);
+
+    /**
+     * averageMiles will input the new mile and will average it with the existing mile
+     * @param newMiles is the miles we need to include in the average
+     */
+    void averageMiles(double newMiles);
+
+    /**
+     * averageTime will input the new time and will average it with the existing time
+     * @param newTime is the time we need to include in the average
+     */
+    void averageTime(double newTime);
+
+private:
+    double fare_;
+    double miles_;
+    double time_;
 };
