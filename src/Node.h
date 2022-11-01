eb7b711fd8e72d668ac93669e07a42a7f70b821a
Megan Ja <meganja2@illinois.edu>
2022-11-01 13:22:44 +0000

    added documentation to the node class

--- src/Node.h
+++ src/Node.h
@@ -4,12 +4,22 @@
 #include <map>
 #include <string>
 
-class Node {
-    public:
-        std::map<Node, Edge>* getNeighbors();
-        std::string getZone();
-    private:
-        std::string startingLocation_;
-        std::map<Node, Edge> neighbors_;
+class Node
+{
+public:
+    /**
+     * getNeighbors() will return an entire list of neighbors.
+     * @return a pointer to a map of nodes and edges.
+     */
+    std::map<Node, Edge> *getNeighbors();
 
+    /**
+     * getZone() will retrieve the zone name
+     * @return startingLocation_
+     */
+    std::string getZone();
+
+private:
+    std::string startingLocation_;
+    std::map<Node, Edge> neighbors_;
 };
