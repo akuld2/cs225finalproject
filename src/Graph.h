5eb178276f32b94b3c09afdf6b78e49917fdaf9a
Megan Ja <meganja2@illinois.edu>
2022-11-08 18:50:17 +0000

    created a struct

--- src/Graph.h
+++ src/Graph.h
@@ -11,20 +11,41 @@
  */
 class Graph
 {
-  public:
-      /**
-       * getNodes() will return an entire list of nodes.
-       * @return a pointer to a vector of nodes.
-       */
-      std::vector<Node> *getNodes();
+private:
+    struct TaxiTripDetails
+    {
+        double tripSecond;
+        double tripMiles;
+        double tripFare;
+        std::string pickupLocation;
+        std::string dropoffLocation;
+        /**
+         * Taxi constructor
+         * @param second is the seconds for the trip
+         * @param miles is the miles traveled for the trip
+         * @param fare is the fare for the trip
+         * @param pickup is the pickup location
+         * @param dropoff is the dropoff location 
+         */
+        TaxiTripDetails(double second, double miles, double fare, std::string pickup, std::string dropoff) : tripSecond(second), tripMiles(miles), tripFare(fare), pickupLocation(pickup), dropoffLocation(dropoff) {}
+    };
 
-      /**
-       * getNode() will retrieve a reference to a node and its content.
-       * @param zone the name of the zone
-       * @return a pointer to a node with the matching zone
-       */
-      Node *getNode(std::string zone);
+public:
+        std::vector<TaxiTripDetails> read_csv(std::string filename);
 
-  private:
-      std::vector<Node> nodes_;
+    /**
+     * getNodes() will return an entire list of nodes.
+     * @return a pointer to a vector of nodes.
+     */
+    std::vector<Node> *getNodes();
+
+    /**
+     * getNode() will retrieve a reference to a node and its content.
+     * @param zone the name of the zone
+     * @return a pointer to a node with the matching zone
+     */
+    Node *getNode(std::string zone);
+
+private:
+    std::vector<Node> nodes_;
 };
